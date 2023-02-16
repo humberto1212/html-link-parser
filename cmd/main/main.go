b4fe15b5dcdf63adfdb5326d5410c933aab8826d
humberto1212 <angeluccirincon@gmail.com>
2023-02-16 17:13:53 +0100

    read fie, parse file, search for links,  print links and print text

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
 func main() {
+
+	// Read file
+	bs, err := ioutil.ReadFile("ADD FILE PATH HERE")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//var vals []string
+	var isLink bool
+
+	// Parse file
+	//tkn := html.NewTokenizer(strings.NewReader(string(bs)))
+	tkn := html.NewTokenizer(strings.NewReader(string(bs)))
+
+	for {
+		tt := tkn.Next()
+
+		switch {
+
+		case tt == html.ErrorToken:
+			return
+		case tt == html.StartTagToken:
+			t := tkn.Token()
+
+			isLink = t.Data == "a"
+			if isLink {
+				fmt.Println("there is a link", t.Data)
+			}
+
+		case tt == html.TextToken:
+
+			t := tkn.Token()
+
+			if isLink {
+				//vals = append(vals, t.Data)
+				//fmt.Println(vals)
+				fmt.Println(t.Data)
+			}
+
+		}
+
+	}
 
 }
