f231c476376b9c9a2819b62fefee576351261a46
humberto1212 <angeluccirincon@gmail.com>
2023-02-17 15:19:08 +0100

    add descrp. comments, add bool values, search for open/close tags, append values to a slice

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	//"htmlLinkParser/models"
 	"io/ioutil"
 	"log"
 	"strings"
@@ -11,19 +12,34 @@ import (
 
 func main() {
 
-	// Read file
-	bs, err := ioutil.ReadFile("ADD FILE PATH HERE")
+	// =======================
+	// 		Read html file
+	// =======================
+	bs, err := ioutil.ReadFile("C:\\Users\\hangelucci\\Desktop\\playground\\go-exer\\html-link-parser\\template\\index.html")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	//var vals []string
+	// =======================
+	//		Conditionals
+	//========================
 	var isLink bool
+	var isEndLink bool
+	isLinkN := 0
+	isEndLinkN := 0
 
-	// Parse file
-	//tkn := html.NewTokenizer(strings.NewReader(string(bs)))
+	//========================
+	//  	Map
+	//========================
+	var v []string
+	//vals := make(map[string]models.LinksList)
+
+	//========================
+	// 		Parse File
+	//========================
 	tkn := html.NewTokenizer(strings.NewReader(string(bs)))
 
+	// START LOOP
 	for {
 		tt := tkn.Next()
 
@@ -33,36 +49,87 @@ func main() {
 			return
 		case tt == html.StartTagToken:
 			t := tkn.Token()
+
 			// ------------ Search <a> ---------
 			isLink = t.Data == "a"
+
+			if t.Data == "a" {
+				// FOUND <a> IS ON
+				isLinkN = 1
+				// CLEAN VALUE AND START OVER
+				isEndLinkN = 0
+			}
+
 			if isLink {
-				fmt.Println("there is a link", t.Data)
+				fmt.Println(t.Data)
 			}
 
 			// ------------ Print the value of href ---------
 			for _, a := range t.Attr {
 				if a.Key == "href" {
 					fmt.Println("Found href:", a.Val)
+					v = append(v, a.Val)
 					break
 				}
 			}
 
-			// case tt == html.TextToken:
+		case tt == html.EndTagToken:
+			t := tkn.Token()
 
-			// 	t := tkn.Token()
+			isEndLink = t.Data == "a"
 
-			// 	if isLink {
-			// 		// 		//vals = append(vals, t.Data)
-			// 		// 		//fmt.Println(vals)
+			if isEndLink {
+				fmt.Println(t.Data)
+			}
 
-			// 		//fmt.Println(t.Data)
+			if t.Data == "a" {
+				// FOUND </a> IS ON
+				isEndLinkN = 1
+				// CLEAN VALUE AND START OVER
+				isLinkN = 0
+			}
 
-			// 		fmt.Println(t)
+		case tt == html.TextToken:
 
-			// 	}
+			t := tkn.Token()
 
+			//vals = append(vals, t.Data)
+
+			if isLinkN == 1 && isEndLinkN == 0 {
+				//fmt.Println(t.Data)
+				v = append(v, t.Data)
+			}
+
+			// ***========== FIX ============***
+			fmt.Println(v)
+			// ***========== FIX ============***
 		}
-
 	}
 
 }
+
+/*
+=====================================================================================
+	Example:
+
+	Link{
+	Href: "/dog",
+	Text: "Something in a span Text not in a span Bold text!",
+	}
+
+	=>
+
+	*** {
+			href: "/cat",
+			Text: " other span text other Text not in span other Bold text HI!"
+	},
+	*** {
+			href: "/dog",
+			Text: " other span text other Text not in span other Bold text HI!"
+	}
+
+	=
+
+	JSON
+=====================================================================================
+*/
